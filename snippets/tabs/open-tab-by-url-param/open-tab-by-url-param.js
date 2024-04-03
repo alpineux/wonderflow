9433b55cc7676cc44765b3bc4c5d9ae29061c5ff
alpineux <99145815+alpineux@users.noreply.github.com>
2024-04-02 22:15:25 -0600

    Query param switching
    Added query param switching on tab change

--- snippets/tabs/open-tab-by-url-param/open-tab-by-url-param.js
+++ snippets/tabs/open-tab-by-url-param/open-tab-by-url-param.js
@@ -1,11 +1,16 @@
 var Webflow = Webflow || [];
 Webflow.push(function () {
-  var tabName = param("CHANGE_ME");
+  const target = "CHANGE_ME";
+  const tabName = param(target);
   if (!tabName) return;
   $('[data-w-tab="' + tabName + '"]').triggerHandler("click");
 
+  $("[data-w-tab]").on("click", function () {
+    history.pushState({}, "", "?" + target + "=" + $(this).data("w-tab"));
+  });
+
   function param(name) {
-    const query = window.location.search;
-    return new URLSearchParams(query);
+    const getParam = new URLSearchParams(document.location.search);
+    return getParam.get(name);
   }
 });
